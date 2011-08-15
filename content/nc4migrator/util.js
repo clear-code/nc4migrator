86a7c02c9390952008184b01e17685102635d768
mooz <stillpedant@gmail.com>
2011-08-15 17:20:06 +0900

    Add http

--- content/nc4migrator/util.js
+++ content/nc4migrator/util.js
@@ -5,7 +5,12 @@ const Ci = Components.interfaces;
 const Cu = Components.utils;
 const Cr = Components.results;
 
+const Application = Cc['@mozilla.org/steel/application;1']
+  .getService(Ci.steelIApplication);
+
 var Util = {
+  DEBUG: false,
+
   getEnv: function (aName, aDefault) {
     var env = Cc['@mozilla.org/process/environment;1']
       .getService(Ci.nsIEnvironment);
@@ -98,5 +103,71 @@ var Util = {
 
   writeJSON: function (aTarget, aObject) {
     return Util.writeFile(aTarget, JSON.stringify(aObject));
+  },
+
+  log: function () {
+    if (this.DEBUG)
+      Application.console.log.apply(Application.console, arguments);
+  },
+
+  http: {
+    params:
+    function params(prm) {
+      let pt = typeof prm;
+
+      if (prm && pt === "object")
+        prm = [k + "=" + v for ([k, v] in Iterator(prm))].join("&");
+      else if (pt !== "string")
+        prm = "";
+
+      return prm;
+    },
+
+    request:
+    function request(method, url, callback, params, opts) {
+      opts = opts || {};
+
+      let req = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
+      req.QueryInterface(Ci.nsIXMLHttpRequest);
+
+      const async = (typeof callback === "function");
+
+      if (async)
+        req.onreadystatechange = function () { if (req.readyState === 4) callback(req); };
+
+      req.open(method, url, async, opts.username, opts.password);
+
+      if (opts.raw)
+        req.overrideMimeType('text/plain; charset=x-user-defined');
+
+      for (let [name, value] in Iterator(opts.header || {}))
+        req.setRequestHeader(name, value);
+
+      req.send(params || null);
+
+      return async ? void 0 : req;
+    },
+
+    get:
+    function get(url, callback, params, opts) {
+      params = this.params(params);
+      if (params)
+        url += "?" + params;
+
+      return this.request("GET", url, callback, null, opts);
+    },
+
+    post:
+    function post(url, callback, params, opts) {
+      params = this.params(params);
+
+      opts = opts || {};
+      opts.header = opts.header || {};
+      opts.header["Content-type"] = "application/x-www-form-urlencoded";
+      opts.header["Content-length"] = params.length;
+      opts.header["Connection"] = "close";
+
+      return this.request("POST", url, callback, params, opts);
+    }
   }
 };

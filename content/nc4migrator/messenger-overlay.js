011a6b748e9906eee3b081362df8b94af942071b
mooz <stillpedant@gmail.com>
2011-10-12 13:40:46 +0900

    Migrate when main window is ready

--- content/nc4migrator/messenger-overlay.js
+++ content/nc4migrator/messenger-overlay.js
@@ -4,6 +4,8 @@ var Nc4Migrator = (function () {
   const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
 
   const { MigrationManager } = Cu.import('resource://nc4migrator-modules/MigrationManager.js', {});
+  const { Util } = Cu.import('resource://nc4migrator-modules/Util.js', {});
+  const { Services } = Cu.import('resource://nc4migrator-modules/Services.js', {});
 
   exports.beginMigration = function () {
     MigrationManager.beginMigration();
@@ -18,6 +20,30 @@ var Nc4Migrator = (function () {
       accountManager.removeAccount(account);
     }
   };
+
+  if (!MigrationManager.mainWindowOpened) {
+    MigrationManager.mainWindowOpened = true;
+
+    // http://mxr.mozilla.org/comm-central/source/mailnews/base/public/nsIMsgIncomingServer.idl#105
+    const concreteAccountTypes = {
+      "pop3": true,
+      "imap": true,
+      "nntp": true
+    };
+
+    let concreteAccounts = Util.toArray(
+      Services.accountManager.accounts, Ci.nsIMsgAccount
+    ).filter(
+      function (account) account.incomingServer
+        && concreteAccountTypes.hasOwnProperty(account.incomingServer.type)
+    );
+
+    if (!concreteAccounts.length) {
+      exports.beginMigration().error(function (x) {
+        Util.log(x);
+      });
+    }
+  }
 
   return exports;
 })();
